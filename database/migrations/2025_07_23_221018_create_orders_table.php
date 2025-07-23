bf125e37f208ab2de55e5e09c9cba1819ab01247
heuderdev <heuderdev@gmail.com>
2025-07-23 19:12:06 -0300

    feat: Criando Model e Migration de Orders

---
+++ database/migrations/2025_07_23_221018_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                  ->nullable()
+                  ->constrained('users')
+                  ->onDelete('set null');
+            $table->decimal('total_amount', 10, 2);
+            $table->string('status')->default('pending');
+            $table->foreignId('shipping_address_id')
+                  ->constrained('addresses') 
+                  ->onDelete('restrict');
+            $table->string('payment_method')->nullable();
+            $table->string('payment_status')->default('unpaid');
+            $table->string('transaction_id')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
