2a98150b7f9edf4396cff63ed457035cddbeee61
heuderdev <heuderdev@gmail.com>
2025-07-23 18:48:54 -0300

    feat: criando os components livewere

---
+++ app/Livewire/Shop/ProductDetails.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Shop;
+
+use Livewire\Component;
+
+class ProductDetails extends Component
+{
+    public function render()
+    {
+        return view('livewire.shop.product-details');
+    }
+}
