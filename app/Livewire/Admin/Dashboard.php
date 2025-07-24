85ab5bbe419d0677bf8118136623f50af11f6a94
heuderdev <heuderdev@gmail.com>
2025-07-24 01:17:27 -0300

    feat: fazendo referência ao layout dashboard

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire\Admin;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    #[Layout('layouts.dashboard')]
     public function render()
     {
         return view('livewire.admin.dashboard');
