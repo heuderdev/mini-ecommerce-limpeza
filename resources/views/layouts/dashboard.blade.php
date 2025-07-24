12ab8c56306c5d59db1d0738e410d3d2c68b1296
heuderdev <heuderdev@gmail.com>
2025-07-24 01:17:49 -0300

    feat: criando o layout de dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,36 +1,44 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            <livewire:layout.navigation />
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @stack('styles')
+</head>
+<body class="font-sans antialiased  min-h-screen">
+<div class="flex min-h-screen" x-data="{ isOpenMenu: window.innerWidth > 770 }"
+    x-init="() => {
+    const updateMenuState = () => {
+             isOpenMenu = window.innerWidth > 768;
+         };
+         updateMenuState();
+         window.addEventListener('resize', () => {
+             clearTimeout(window.resizedFinished);
+             window.resizedFinished = setTimeout(updateMenuState, 100);
+         });
+    }"
+>
+    <aside
+        :class="{ 'hidden': !isOpenMenu, 'block': isOpenMenu }"
+        class="fixed top-0 bottom-0 bg-[#0a0a0a] w-24 hidden md:block" x-show="isOpenMenu" x-transition>
+        <div class="">
+            @livewire('helpers.dashboard-navigation')
         </div>
-    </body>
+    </aside>
+    <main class="bg-purple-50 flex-1 p-4">
+        <button @click="isOpenMenu = !isOpenMenu" class="block md:hidden">
+            <x-menu class="w-8 h-8" />
+        </button>
+        {{ $slot }}
+    </main>
+</div>
+@stack('scripts')
+</body>
 </html>
