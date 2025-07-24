1013d04a39c90a959a5e5de93a13fa72bbb6f920
heuderdev <heuderdev@gmail.com>
2025-07-24 01:16:46 -0300

    feat: renomeando e adicionando routa de dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
+use App\Livewire\Admin\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
+Route::view('painel', 'dashboard')
     ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+    ->name('painel');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::get("/dashboard", Dashboard::class)->name('dashboard');
 
 require __DIR__.'/auth.php';
